aba23ce2ca60ecbec9af079a44e7b28aedba4d0b
bhaskarayh <bhaskarayh@gmail.com>
2025-03-21 10:55:40 +0700

    Implements Eureka Server and Client

--- healthprovider-service/src/main/java/com/devzees/healthprovider/HealthproviderServiceApplication.java
+++ healthprovider-service/src/main/java/com/devzees/healthprovider/HealthproviderServiceApplication.java
@@ -2,9 +2,11 @@ package com.devzees.healthprovider;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 @ComponentScan(basePackages = {"com.devzees"})
 public class HealthproviderServiceApplication {
 
